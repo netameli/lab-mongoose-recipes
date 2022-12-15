8cd7525131a0f37707bdacfd36f37269ab7f2a10
Janset Genel <jansetgenel@gmail.com>
2022-12-15 17:24:20 +0100

    final try

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://localhost:27017/first-database';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -19,15 +19,28 @@ mongoose
       Recipe.create({ title, level, ingredients, cuisine, dishType, image, duration, creator, created } )
       console.log(title)
   })
-  
+  .then(() => {
   Recipe.findOneAndUpdate(query, { title: 'Rigatoni alla Genovese' }, options, callback)
   Recipe.findOneAndUpdate(query, { $set: { duration: '100' }}, options, callback)
   Recipe.deleteOne({ title: 'Carrot Cake' })
   .then(successCallback)
   .catch(errorCallback);
-   
-    mongoose.connection.close();
-    
+})
+    const myRecipe = {
+      title: 'Manti'
+      level: 'UltraPro Chef'
+      ingredients: '[ potatoe, flour, water, egg ] '
+      cuisine: 'Turkish'
+      dishType: [main_course], 
+      duration: '100'
+      creator: 'Janset'
+  return Recipe.create(myRecipe)
+})
+
+.then(newRecipe ==> console={.log ('Recipe succesfully created', newRecipe.title)})
+
+mongoose.connection.close();
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
